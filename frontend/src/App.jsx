a2c2dbd65fba5ba902d3dfa3267661f2900430d2
Rishi <rishi.i3468@gmail.com>
2025-08-23 02:39:49 +0530

    minor fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Routes} from 'react-router'
 import AgriConnect from './pages/HomePage'
 import MarketPlace from './pages/MarketplacePage'
+import PaymentPage from './pages/PaymentPage'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<AgriConnect />}/>
           <Route path="/marketplace" element={<MarketPlace />} />
+          <Route path="/payment" element={<PaymentPage />} />
         </Routes>
       </div>
     </div>
