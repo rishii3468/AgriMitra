9254acfedeae101581e5b06d6c52f7c1cd67c5db
Rishi <rishi.i3468@gmail.com>
2025-11-16 23:38:13 +0530

    minor fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://agrimitra-1-hmee.onrender.com/api";  
 
 const api = axios.create({
-    baseURL: BASE_URL,
-
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
 
 export default api;
